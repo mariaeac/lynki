921f305f92804c1a01f388a0b9b6775f1f94cdad
Maria Eduarda <meac21kj@gmail.com>
2025-05-08 04:02:14 -0300

    feat(docker): Add Docker-compose file with MongoDB configuration

---
+++ src/main/java/com/lynki/lynki/LynkiApplication.java
@@ -0,0 +1,13 @@
+package com.lynki.lynki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LynkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LynkiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lynki/lynki/LynkiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lynki.lynki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LynkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
