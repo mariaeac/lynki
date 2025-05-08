4a4d96333c79265b6269a70c4e41f836f546f333
Maria Eduarda <meac21kj@gmail.com>
2025-05-08 05:33:23 -0300

    fix(repository): UrlRepository key type

--- src/main/java/com/lynki/lynki/repository/UrlRepository.java
+++ src/main/java/com/lynki/lynki/repository/UrlRepository.java
@@ -1,11 +1,8 @@
 package com.lynki.lynki.repository;
-
-
 import com.lynki.lynki.model.Url;
 import org.springframework.data.mongodb.repository.MongoRepository;
-
 import java.util.UUID;
 
-public interface  UrlRepository extends MongoRepository<Url, UUID> {
+public interface  UrlRepository extends MongoRepository<Url, String> {
 
 }
