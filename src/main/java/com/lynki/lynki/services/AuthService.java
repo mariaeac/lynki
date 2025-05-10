24c30faf5b764e961652a7246597f628fa5a0786
Maria Eduarda <meac21kj@gmail.com>
2025-05-10 11:53:56 -0300

    feat(services): Add AuthService

---
+++ src/main/java/com/lynki/lynki/services/AuthService.java
@@ -0,0 +1,65 @@
+package com.lynki.lynki.services;
+
+import com.lynki.lynki.domain.User;
+import com.lynki.lynki.domain.dtos.LoginRequestDTO;
+import com.lynki.lynki.domain.dtos.LoginResponseDTO;
+import com.lynki.lynki.domain.dtos.RegisterUserDTO;
+import com.lynki.lynki.domain.enums.UserRole;
+import com.lynki.lynki.exceptions.UserException;
+import com.lynki.lynki.infra.TokenService;
+import com.lynki.lynki.repository.UserRepository;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+public class AuthService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
+
+    public AuthService(UserRepository userRepository, @Lazy AuthenticationManager authenticationManager, TokenService tokenService) {
+        this.userRepository = userRepository;
+        this.authenticationManager = authenticationManager;
+        this.tokenService = tokenService;
+    }
+
+
+    public void registerUser(RegisterUserDTO registerUser) {
+
+        if (userRepository.findByUsername(registerUser.username()) != null) {
+           throw new UserException.UsernameAlreadyExists(registerUser.username());
+        }
+
+        String encryptedPassword = new BCryptPasswordEncoder().encode(registerUser.password());
+        userRepository.save(new User(registerUser.username(),encryptedPassword, registerUser.email(), UserRole.ADMIN));
+
+    }
+
+    public LoginResponseDTO login(LoginRequestDTO loginRequest) {
+
+        UsernamePasswordAuthenticationToken usernamePassword = new UsernamePasswordAuthenticationToken(loginRequest.username(), loginRequest.password());
+        Authentication authentication = authenticationManager.authenticate(usernamePassword);
+        String token = tokenService.generateToken((User) authentication.getPrincipal());
+
+        return new LoginResponseDTO(token);
+
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username);
+    }
+
+
+}
