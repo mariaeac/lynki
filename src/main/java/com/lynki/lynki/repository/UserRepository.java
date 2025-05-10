671610eb332c3d78918fe43741b1efe501cc4c5d
Maria Eduarda <meac21kj@gmail.com>
2025-05-10 11:49:09 -0300

    feat(repository): Add User repository

---
+++ src/main/java/com/lynki/lynki/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.lynki.lynki.repository;
+
+import com.lynki.lynki.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, String> {
+
+    User findByUsername(String username);
+}
