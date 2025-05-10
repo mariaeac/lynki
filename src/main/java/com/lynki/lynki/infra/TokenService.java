0e9b2fd268ba89117016791b7413de68a707bd90
Maria Eduarda <meac21kj@gmail.com>
2025-05-10 11:57:01 -0300

    feat(service): Add TokenService

---
+++ src/main/java/com/lynki/lynki/infra/TokenService.java
@@ -0,0 +1,52 @@
+package com.lynki.lynki.infra;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.lynki.lynki.domain.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public String generateToken(User user) {
+
+        try {
+
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            String jwtToken = JWT.create().withIssuer("lynki-api")
+                    .withSubject(user.getUsername())
+                    .withExpiresAt(generateExpirationTime())
+                    .sign(algorithm);
+            return jwtToken;
+        } catch (JWTCreationException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm).withIssuer("lynki-api").build().verify(token).getSubject();
+
+        } catch (JWTVerificationException e) {
+            return "";
+        }
+    }
+
+    private Instant generateExpirationTime() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+
+    }
+
+
+}
